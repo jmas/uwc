647dd9978231ac695c36f9b52f851c92ab248dfa
Ourmaker <outmaker@mailinator.com>
2014-11-02 21:51:34 +0200

    Added ES5 shim for IE.

--- build-front.sh
+++ build-front.sh
@@ -6,3 +6,8 @@ cp -f front/index.html www/index.html
 cp -f front/favicon.ico www/favicon.ico
 cp -fr front/js/ www/js/
 cp -fr front/images/ www/images/
+
+# Minify
+# minify www/index.js www/index.js
+# minify www/styles.css www/styles.css
+
